189803f3c5f5419003498a772262ed3e648098eb
unknown <rafinhakafka@gmail.com>
2021-07-14 21:55:00 -0300

    First commit

---
+++ src/main/java/br/com/fiap/config/Constants.java
@@ -0,0 +1,11 @@
+package br.com.fiap.config;
+
+/**
+ * Application constants.
+ */
+public final class Constants {
+
+    public static final String SYSTEM = "system";
+
+    private Constants() {}
+}
